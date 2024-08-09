664f4fc6cd013007f7283adadfc76bf8a1cb0be0
rmadmsjm <dmswjdals12@naver.com>
2024-08-09 16:34:26 +0900

    CatalogApp, LazyColumn

---
+++ JC17_CatalogApp/app/src/main/java/kr/co/rmadmsjm/jc17_catalogapp/MainActivity.kt
@@ -0,0 +1,181 @@
+package kr.co.rmadmsjm.jc17_catalogapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import kr.co.rmadmsjm.jc17_catalogapp.ui.theme.JC17_CatalogAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            JC17_CatalogAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    CatalogEx(items)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Item(itemData: ItemData) {
+    // 1. catalog1.png 참고해 카드 레이아웃 완성
+    Card(
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 8.dp
+        ),
+        colors = CardDefaults.cardColors(
+            containerColor = Color.White
+        ),
+        modifier = Modifier.padding(16.dp)
+    ) {
+        Column(
+            verticalArrangement = Arrangement.Center,
+            modifier = Modifier.padding(16.dp)
+        ) {
+            Image(
+                painter = painterResource(
+                    id = itemData.imageId
+                ),
+                contentDescription = "이미지"
+            )
+            Spacer(modifier = Modifier.size(12.dp))
+            Text(
+                text = itemData.title,
+                style = MaterialTheme.typography.headlineLarge
+            )
+            Spacer(modifier = Modifier.size(8.dp))
+            Text(
+                text = itemData.description,
+                style = MaterialTheme.typography.bodyLarge.copy(lineHeight = 20.sp),
+                textAlign = TextAlign.Justify
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ItemPreview() {
+    JC17_CatalogAppTheme {
+        Item(
+            ItemData(
+                imageId = R.drawable.a1,
+                title = "해변 놀이 공원",
+                description = "해변 놀이 공원 설명입니다. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. "
+            )
+        )
+    }
+}
+
+@Composable
+fun CatalogEx(itemList: List<ItemData>) {
+    LazyColumn {
+        // 2. `items(itemList)`를 이용해 Item을 반복해서 컬럼에 추가
+        /*
+        item {
+            Item(itemData = itemList[0])
+        }
+        item {
+            Item(itemData = itemList[0])
+        }
+         */
+        items(itemList) {item ->
+            Item(itemData = item)
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    JC17_CatalogAppTheme {
+        CatalogEx(items)
+    }
+}
+
+data class ItemData(
+    @DrawableRes val imageId: Int,
+    val title: String,
+    val description: String,
+)
+
+val items = listOf(
+    ItemData(
+        imageId = R.drawable.a1,
+        title = "해변 놀이 공원",
+        description = "해변 놀이 공원 설명입니다. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. "
+    ),
+    ItemData(
+        imageId = R.drawable.a2,
+        title = "캐년",
+        description = "미국의 캐년입니다. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. "
+    ),
+    ItemData(
+        imageId = R.drawable.a3,
+        title = "워터월드",
+        description = "워터월드입니다. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. "
+    ),
+    ItemData(
+        imageId = R.drawable.a4,
+        title = "미국의 캐년",
+        description = "미국의 캐년입니다. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. "
+    ),
+    ItemData(
+        imageId = R.drawable.a5,
+        title = "라스베가스",
+        description = "라스베가스입니다. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. "
+    ),
+    ItemData(
+        imageId = R.drawable.a6,
+        title = "호르슈 밴드",
+        description = "호르슈 밴드입니다. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. "
+    ),
+    ItemData(
+        imageId = R.drawable.a7,
+        title = "미국의 길",
+        description = "미국의 길입니다. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. "
+    ),
+    ItemData(
+        imageId = R.drawable.a8,
+        title = "엔텔로프 캐년",
+        description = "엔텔로프 캐년입니다. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. "
+    ),
+    ItemData(
+        imageId = R.drawable.a9,
+        title = "그랜드 캐년",
+        description = "그랜드 캐년입니다. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. "
+    ),
+)
